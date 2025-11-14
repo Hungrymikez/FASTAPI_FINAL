8342413d448c60684959162789708792d91b3c85
Hungrymikez <codemashop@gmail.com>
2025-11-14 08:43:25 -0500

    ekisde

--- app/router/usuarios.py
+++ app/router/usuarios.py
@@ -1,4 +1,5 @@
 
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.schemas.usuarios import CrearUsuario, EditarPass, Editar_usuario, RetornoUsuario
