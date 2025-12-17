f0ba24484698be42353e5a92d863852a4fd2dea5
Hungrymikez <codemashop@gmail.com>
2025-12-17 07:16:51 -0500

    Actualizacion del main

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ origins = [
     "http://127.0.0.1:5500", 
     "http://localhost:5500",
     "https://fastapifinal-production.up.railway.app", # Si ya está en producción
+    "https://hungrymikez.github.io/INNOVACION/"
 ]
 
 
